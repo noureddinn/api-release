22ca4b825e5f5daec5abb19e9395dc530f011acf
NOUREDDIN SOUAD <noureddin.souad@damanesign.ma>
2022-01-26 10:03:32 +0100

    dsds

--- src/main/java/org/wyona/webapp/Server.java
+++ src/main/java/org/wyona/webapp/Server.java
@@ -16,9 +16,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 public class Server extends SpringBootServletInitializer {
 
-    /**
-     *
-     */
+
     public static void main(String[] args) {
         SpringApplication.run(Server.class, args);
     }
